596662b96f467baf6444e4d6f65b22035daf70dc
Dmitrii Bogorodskii <DmitriiBoog@gmail.com>
2024-09-17 20:26:20 -0400

    I finished this one and focus to my Portfolio

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
     const airQualityUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&hourly=pm10,pm2_5`;
 
-    // Get and work Temperatures API
+    // get date for weather
     fetch(weatherUrl)
       .then((response) => response.json())
       .then((data) => {
@@ -25,21 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         }
 
-        const cityDisplay = `<div class="city-card"><h3>Weather for ${cityName}:</h3></div>`;
-        const weatherDetails = `
-          <div class="temperature">
-            <p><strong>Max:</strong> ${maxTemp} °C <strong>Data:</strong> ${maxTempTime}</p>
-            <p><strong>Min:</strong> ${minTemp} °C <strong>Data:</strong> ${minTempTime}</p>
-          </div>`;
+        const cityDisplay = `<div class="city-card">
+        <h3>Weather for <br>${cityName}:</h3>
+        <div class="temperature">
+          <p><strong>Max Temperature:</strong> ${maxTemp} °C <br> <strong>Time:</strong> ${times[0]}</p>
+          <p><strong>Min Temperature:</strong> ${minTemp} °C <br> <strong>Time:</strong> ${times[0]}</p>
+        </div>
+      </div>`;
 
-        document.getElementById("weather-data").innerHTML +=
-          cityDisplay + weatherDetails;
+        document.getElementById("weather-data").innerHTML += cityDisplay;
       })
       .catch((error) =>
         console.error(`Error fetching data for ${cityName} weather:`, error)
       );
 
-    // Get and work Quality API
+    // get airQuality Date
     fetch(airQualityUrl)
       .then((response) => response.json())
       .then((data) => {
@@ -47,12 +47,16 @@ document.addEventListener("DOMContentLoaded", () => {
         const pm25 = data.hourly.pm2_5[0];
         const airQualityTime = data.hourly.time[0];
 
-        const airQualityDisplay = `<div class="city-card"><h3>Air Quality for ${cityName}:</h3></div>`;
-        const pm10Display = `<p><strong>PM10:</strong> ${pm10} µg/m³ <strong>Data:</strong> ${airQualityTime}</p>`;
-        const pm25Display = `<p><strong>PM2.5:</strong> ${pm25} µg/m³ <strong>Data:</strong> ${airQualityTime}</p>`;
+        const airQualityDisplay = `<div class="city-card">
+        <h3>Air Quality for <br>${cityName}:</h3>
+        <div class="air-quality-details">
+          <p><strong>PM10:</strong> ${pm10} µg/m³ <br> <strong>Time:</strong> ${airQualityTime}</p>
+          <p><strong>PM2.5:</strong> ${pm25} µg/m³ <br> <strong>Time:</strong> ${airQualityTime}</p>
+        </div>
+      </div>`;
 
         document.getElementById("air-quality-data").innerHTML +=
-          airQualityDisplay + pm10Display + pm25Display;
+          airQualityDisplay;
       })
       .catch((error) =>
         console.error(`Error fetching data for ${cityName} air quality:`, error)
@@ -61,17 +65,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // displays (switching)
   document.getElementById("weather-btn").addEventListener("click", () => {
-    document.getElementById("weather-data").style.display = "block";
+    document.getElementById("weather-data").style.display = "grid";
     document.getElementById("air-quality-data").style.display = "none";
   });
 
   document.getElementById("air-quality-btn").addEventListener("click", () => {
     document.getElementById("weather-data").style.display = "none";
-    document.getElementById("air-quality-data").style.display = "block";
+    document.getElementById("air-quality-data").style.display = "grid";
   });
 
   // cities
-  fetchWeatherAndAirQuality(52.52, 13.41, "Berlin");
-  fetchWeatherAndAirQuality(55.75, 37.62, "Moscow");
+  fetchWeatherAndAirQuality(35.68, 139.76, "Tokyo");
+  fetchWeatherAndAirQuality(64.54, 40.52, "Archangelsk");
   fetchWeatherAndAirQuality(35.23, -80.84, "Charlotte NC");
 });
